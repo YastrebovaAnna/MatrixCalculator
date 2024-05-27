3e3afe4bf1a7b7e69a7da90a8dbfdecc94471c28
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-05-27 11:28:36 +0300

    Added interface IMatrix.Using this interface

---
+++ WinFormsApp1/LibraryMatrix/core/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using LibraryMatrix.interfaces;
+
+namespace LibraryMatrix.core
+{
+    public class Matrix : IMatrix
+    {
+        public double[,] MatrixArray { get; private set; }
+
+        public int Rows => MatrixArray.GetLength(0);
+        public int Columns => MatrixArray.GetLength(1);
+
+        public Matrix(int rows, int columns, double[,] matrixArray = null)
+        {
+            MatrixArray = matrixArray ?? new double[rows, columns];
+        }
+    }
+}
--- WinFormsApp1/LibraryMatrix/interfaces/IMatrix.cs
+++ WinFormsApp1/LibraryMatrix/interfaces/IMatrix.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibraryMatrix.interfaces
+﻿namespace LibraryMatrix.interfaces
 {
     public interface IMatrix
     {
         int Rows { get; }
         int Columns { get; }
+        double[,] MatrixArray { get; }
     }
 }
